8d8ea650dc8198a20fac1bc56fb4362055a349a4
mix irving <whimful@gmail.com>
2015-02-10 20:45:51 +1300

    extract checker

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var yaml = require('js-yaml');
 var indent = require('indent-string');
 var sortObj = require('sort-object');
 var style = require('fontStyle');
+var check = require('checkers');
 
 var projectSlug = 'loomio-1'
 var loomioDir = process.argv[2] || '/home/mix/projects/loomio'
@@ -105,8 +106,8 @@ function updateLocale(resource, locale) {
         if (typeof(localeValue) === 'undefined') {return}
 
         var args = { locale: locale, resource: resource, key: key, value: value, localeKey: localeKey, localeValue: localeValue }
-        checkInterpolation(args)
-        checkHTML(args)
+        check.interpolation(args)
+        check.html(args)
       })
     })
 
@@ -117,48 +118,6 @@ function updateLocale(resource, locale) {
       process.stdout.write(style.green('.'))
     })
 
-  }
-}
-
-function checkInterpolation(args) {
-  var locale= args.locale, resource= args.resource, key= args.key, value= args.value, localeKey= args.localeKey, localeValue= args.localeValue;
-  
-  var regex = /%{[^{}]*}/gm
-  var skipKeys = ["en.invitation.invitees_placeholder",]
-
-  var match = value.match(regex)
-  var localeMatch = localeValue.match(regex)
-
-  if (match === null) {return}
-  if (localeMatch === null) {
-    niceLog( {locale: locale, key: key, value: value, localeKey: localeKey, localeValue: localeValue, resource: resource, regex: regex} )
-    return
-  }
-  //
-
-  //note interpolation keys are not order dependent, so we normalise by .sort()'ing
-  if ( (match.sort().join(':') !== localeMatch.sort().join(':'))
-    && (skipKeys.indexOf(key) === -1) ){
-      niceLog( {locale: locale, key: key, value: value, localeKey: localeKey, localeValue: localeValue, resource: resource, regex: regex} )
-  }
-}
-
-function checkHTML(args) {
-  var locale= args.locale, resource= args.resource, key= args.key, value= args.value, localeKey= args.localeKey, localeValue= args.localeValue;
-  
-  var regex = /\<[^\<\>]*\>/gm
-  var skipKeys = ["en.invitation.invitees_placeholder",]
-
-  var match = value.match(regex)
-  var localeMatch = localeValue.match(regex)
-
-  if (match === null) {return}
-  if (localeMatch === null) {throw localeKey}
-
-  //note we don't sort keys because html is order dep. this could be trickier with some languages
-  if ( (match.join(':') !== localeMatch.join(':'))
-    && (skipKeys.indexOf(key) === -1) ){
-      niceLog( {locale: locale, key: key, value: value, localeKey: localeKey, localeValue: localeValue, resource: resource, regex: regex} )
   }
 }
 
