57c911078b81f1b3572fa79ea11efc6d69e63bb9
mix irving <whimful@gmail.com>
2015-02-26 11:07:33 +1300

    extract updateLocale

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-var http    = require('http')
-var request = require('request')
-var fs      = require('fs')
-var flatten = require('flat')
-var yaml    = require('js-yaml')
 
 // my mini modules
 var style            = require('./fontStyle')
@@ -11,6 +6,7 @@ var getFromTransifex = require('./getFromTransifex')
 var fetchStats       = require('./fetchStats')
 var writeStats       = require('./writeStats')
 var print            = require('./stdPrint')
+var updateLocale     = require('./updateLocale')
 
 var config  = require('./config');
   var projectSlug = config.projectSlug;
@@ -42,50 +38,6 @@ function updateLocales(locales) {
       getFromTransifex("/api/2/project/"+projectSlug+"/resource/"+resource.transifexSlug+"/translation/"+locale)(updateLocale(resource, locale))
     })
   })
-}
-
-function updateLocale(resource, locale) {
-  return function(err, res, body) {
-    if (err) { throw err; }
-    
-    var correctedLocale = locale.replace('_','-')
-    var filename = localesDir + resource.localFilePrefix + correctedLocale + '.yml'
-    var refFile = localesDir + resource.localFilePrefix + 'en.yml'
-
-    // get and correct the transifex translation
-    var tempTransifexTranslations = yaml.safeLoad(JSON.parse(body).content)
-    var transifexTranslations = {}
-    transifexTranslations[correctedLocale] = tempTransifexTranslations[locale]
-    
-    // load loomio translation and compare
-    fs.readFile(refFile, function(err, data) {
-      if (err) { throw err; }
-
-      var flatRefTranslations = flatten(yaml.safeLoad(data))
-      var flatTransifexTranslations = flatten(transifexTranslations)
-     
-      Object.keys(flatRefTranslations).forEach( function(key) {
-        var localeKey = key.replace('en.', correctedLocale+'.')
-
-        var value = flatRefTranslations[key]
-        var localeValue = flatTransifexTranslations[localeKey]
-
-        if (typeof(localeValue) === 'undefined') {return}
-
-        var args = { locale: locale, resource: resource, key: key, value: value, localeKey: localeKey, localeValue: localeValue }
-        check.interpolation(args)
-        check.html(args)
-      })
-    })
-
-    fs.writeFile(filename, yaml.safeDump(transifexTranslations), function(err) {
-      if (err) { throw err; }
-
-      //process.stdout.write(style.green(locale)+' ')
-      process.stdout.write(style.green('.'))
-    })
-
-  }
 }
 
 
---
+++ updateLocale.js
@@ -0,0 +1,54 @@
+var flatten = require('flat')
+var fs = require('fs')
+var yaml    = require('js-yaml')
+
+var check = require('./checkers')
+var print = require('./stdPrint')
+var style = require('./fontStyle')
+var config  = require('./config');
+  var localesDir  = config.localesDir;
+
+module.exports = function updateLocale(resource, locale) {
+  return function(err, res, body) {
+    if (err) { throw err; }
+    
+    var correctedLocale = locale.replace('_','-')
+    var filename = localesDir + resource.localFilePrefix + correctedLocale + '.yml'
+    var refFile = localesDir + resource.localFilePrefix + 'en.yml'
+
+    // get and correct the transifex translation
+    var tempTransifexTranslations = yaml.safeLoad(JSON.parse(body).content)
+    var transifexTranslations = {}
+    transifexTranslations[correctedLocale] = tempTransifexTranslations[locale]
+    
+    // load loomio translation and compare
+    fs.readFile(refFile, function(err, data) {
+      if (err) { throw err; }
+
+      var flatRefTranslations = flatten(yaml.safeLoad(data))
+      var flatTransifexTranslations = flatten(transifexTranslations)
+     
+      Object.keys(flatRefTranslations).forEach( function(key) {
+        var localeKey = key.replace('en.', correctedLocale+'.')
+
+        var value = flatRefTranslations[key]
+        var localeValue = flatTransifexTranslations[localeKey]
+
+        if (typeof(localeValue) === 'undefined') {return}
+
+        var args = { locale: locale, resource: resource, key: key, value: value, localeKey: localeKey, localeValue: localeValue }
+        check.interpolation(args)
+        check.html(args)
+      })
+    })
+
+    fs.writeFile(filename, yaml.safeDump(transifexTranslations), function(err) {
+      if (err) { throw err; }
+
+      //process.stdout.write(style.green(locale)+' ')
+      print(style.green('.'))
+    })
+
+  }
+}
+
